57d805ad75c128b72a41696e1e31e768c3467e69
Minah Kim <kimma1205@gmail.com>
2026-03-04 17:59:23 +0900

    VideoCard 컴포넌트의 인라인 css 를 클래스화 하여 리팩토링

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -5,25 +5,27 @@ const VideoCard = ({ video }: { video: Video }) => {
     return (
         <div className="video-card-container">
             {/* 썸네일 */}
-            <div style={{ padding: "0 0 12px 0" }}>
-                <img style={{ width: "100%", borderRadius: "12px" }} src={video.thumbnailUrl} />
+            <div className="video-card-thumbnail">
+                <img src={video.thumbnailUrl} />
             </div>
             {/* 카드 */}
-            <div style={{ display: "flex" }}>
+            <div className="video-card-info">
                 {/* 로고 */}
-                <div style={{ width: "36px", margin: "0 12px 0 0" }}>
-                    <img style={{ width: "100%", borderRadius: "50%" }} src="https://yt3.ggpht.com/by9Fh1prHd_mmc52XM3nscTy2U7qssX0D3sE4w2e6OQAIlEz3wE3MRFImIk9L-xoWtSSYmgkQg=s68-c-k-c0x00ffffff-no-rj"></img>
+                <div className="video-card-avatar">
+                    <img src="https://yt3.ggpht.com/by9Fh1prHd_mmc52XM3nscTy2U7qssX0D3sE4w2e6OQAIlEz3wE3MRFImIk9L-xoWtSSYmgkQg=s68-c-k-c0x00ffffff-no-rj" />
                 </div>
                 {/* 상세 */}
                 <div>
-                    <div><span style={{ fontSize: "16px", fontWeight: "500" }}>{video.title}</span></div>
                     <div>
-                        <span style={{ fontSize: "14px", fontWeight: "400", color: "rgb(96, 96, 96)" }}>{video.channelName}</span>
+                        <span className="video-card-title">{video.title}</span>
                     </div>
                     <div>
-                        <span style={{ fontSize: "14px", fontWeight: "400", color: "rgb(96, 96, 96)" }}>{video.viewCount}</span>
-                        <span style={{ fontSize: "14px", fontWeight: "400", color: "rgb(96, 96, 96)" }}> • </span>
-                        <span style={{ fontSize: "14px", fontWeight: "400", color: "rgb(96, 96, 96)" }}>{video.uploadedAt}</span>
+                        <span className="video-card-meta">{video.channelName}</span>
+                    </div>
+                    <div>
+                        <span className="video-card-meta">{video.viewCount}</span>
+                        <span className="video-card-meta"> • </span>
+                        <span className="video-card-meta">{video.uploadedAt}</span>
                     </div>
                 </div>
             </div>
