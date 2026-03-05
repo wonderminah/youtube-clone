7c055e29a3f4e66dea63ce7985f002b2cd4b6607
Minah Kim <kimma1205@gmail.com>
2026-03-05 14:48:19 +0900

    인라인 스타일을 css 클래스로 정의하고 모듈화

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-import "./Sidebar.module.css"
+import styles from "./Sidebar.module.css"
 import { mockSubscriptions } from "../../mocks/subscriptions"
 import SidebarItem from "./SidebarSections/SidebarItem"
 import SidebarTitle from "./SidebarSections/SidebarTitle"
@@ -7,14 +7,10 @@ import { sidebarSections } from "./SidebarSections/sidebarSections"
 
 const Sidebar = () => {
   return (
-    <div style={{
-      width: "240px",
-      flexShrink: 0,
-      padding: "12px",
-    }}>
+    <div id="sidebar" className={styles.sidebarContainer}>
       {sidebarSections.map((section, sectionIndex) => (
         <Fragment key={sectionIndex}>
-          <div className="sidebar-section">
+          <div className={styles.sidebarSection}>
             {section.title && <SidebarTitle title={section.title} />}
             {section.items.map((item, itemIndex) => (
               <SidebarItem key={itemIndex} iconNode={item.iconNode} name={item.name} />
@@ -23,7 +19,7 @@ const Sidebar = () => {
 
           {/* Subscriptions 섹션을 표시 */}
           {sectionIndex === 0 && (
-            <div className="sidebar-section">
+            <div className={styles.sidebarSection}>
               <SidebarTitle title="Subscriptions" />
               {mockSubscriptions.map((channel, index) => (
                 <SidebarItem key={index} imgUrl={channel.avatarUrl} name={channel.name} hasNewVideo={channel.hasNewVideo} />
